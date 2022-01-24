23ce86fa5ce05a010934e0cee0c1d5b3154baf7d
cdmejiam94 <cdmejiam94@gmail.com>
2022-01-24 01:28:04 -0600

    Se agrego metodo para dar formato a respuestas; se agrego campo project_name

--- controllers/session-history.controller.js
+++ controllers/session-history.controller.js
@@ -1,21 +1,24 @@
-const { json } = require('body-parser');
 const firestore = require('../config/db');
 const SessionHistory = require('../models/session-history');
+const ApiResponse = require('../models/api-response')
 
 class SessionHistoryController{
-  constructor() {}
+  constructor() {
+    this.collection = "session-history"
+    this.response = new ApiResponse()
+  }
 
   async addHistoryEntry (data) {
-    await firestore.collection("session-history").doc().set(data);
-    return data;
+    await firestore.collection(this.collection).doc().set(data);
+    return this.response.toApiResponse(this.collection,[data],"Success");
   };
 
   async getAllSessionHistory () {
     const historyList = [];
-    const data = await firestore.collection("session-history").get();
+    const data = await firestore.collection(this.collection).get();
 
     if (data.empty) {
-      return json({message : "No records found"})
+      return this.response.toApiResponseEmpty(this.collection)
     } else {
       data.forEach((item) => {
         const entry = new SessionHistory(
@@ -25,22 +28,23 @@ class SessionHistoryController{
           item.data().time_stamp,
           item.data().pid,
           item.id,
-          item.data().status
+          item.data().status,
+          item.data().project_name
         );
         historyList.push(entry);
       });
 
-      return historyList
+      return this.response.toApiResponse(this.collection, historyList, "Success")
     }
   };
 
   async getAllSessionHistoryByUserId (id) {
-    const entries = firestore.collection("session-history");
+    const entries = firestore.collection(this.collection);
     const data = await entries.where("uid","==",id).get();
     const historyList = [];
 
     if (data.empty) {
-      return json({message : "No records found"})
+      return this.response.toApiResponseEmpty(this.collection)
     } else {
       data.forEach((item) => {
         const entry = new SessionHistory(
@@ -50,22 +54,23 @@ class SessionHistoryController{
           item.data().time_stamp,
           item.data().pid,
           item.id,
-          item.data().status
+          item.data().status,
+          item.data().project_name
         );
         historyList.push(entry);
       });
 
-      return historyList
+      return this.response.toApiResponse(this.collection, historyList, "Success")
     }
   };
 
   async deleteSessionHistoryByStatus (status) {
-    const data = await firestore.collection("session-history")
+    const data = await firestore.collection(this.collection)
                                 .where("status", "==", status)
                                 .get();
 
     if (data.empty) {
-      return {"message": "No records found" }
+      return this.response.toApiResponseEmpty(this.collection)
     } else {
       data.forEach((item) => {
           firestore.collection("session-history")
@@ -73,14 +78,14 @@ class SessionHistoryController{
           .delete()
       });
 
-      return {"message": "Records deleted!"}
+      return this.response.toApiResponse(this.collection,[],"Success");
     }
   }
 
   async updateSessionHistoryEntry (data,id) {
-    const entryToUpdate = firestore.collection("session-history").doc(id);
+    const entryToUpdate = firestore.collection(this.collection).doc(id);
     await entryToUpdate.update(data)
-    return data;
+    return this.response.toApiResponse(this.collection,[data],"Success");;
   };
 }
 
--- models/session-history.js
+++ models/session-history.js
@@ -1,5 +1,5 @@
 class SessionHistory {
-  constructor(admin_id, uid, sid, time_stamp, pid, shid, status) {
+  constructor(admin_id, uid, sid, time_stamp, pid, shid, status, project_name) {
           this.admin_id = admin_id;
           this.uid = uid;
           this.sid = sid;
@@ -7,6 +7,7 @@ class SessionHistory {
           this.pid = pid;
           this.shid = shid;
           this.status = status;
+          this.project_name = project_name;
   }
 }
 
